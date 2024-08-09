5d53e17d863f15c9b6fc9af269fa2da65ecddbd1
Ilya Kondryukov <ilyakondryukov8765@gmail.com>
2024-08-09 17:04:43 +0300

    Многое еще предстоит, толкового ничего не добавил

--- index.php
+++ index.php
@@ -1,15 +1,26 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Crypto-Manager</title>
-    </head>
-    <body>
-        <form method="post" action="process.php">
-            <label for="coin">Монета:</label>
-            <input type="text" id="coin" name="coin" required>
-            <label for="count">Количество:</label>
-            <input type="text" id="count" name="count" required>
-            <input type="submit" value="Отправить">
-        </form>
-    </body>
-</html>
+
+<?php
+
+$coin_list = [
+    'ATOM'=>[50, 50],
+    'DOT'=>[50, 50],
+    'STRK'=>[50, 50],
+    'SOL'=>[50, 50],
+];
+
+$coin_list += array('TON'=>[100, 200]);
+
+print_r($coin_list);
+
+function add_coin($name, $average_price, $number_of_coins) {
+    $coin_list[$name] = [$average_price, $number_of_coins];
+}
+add_coin("TUT", 123, 123);
+print_r($coin_list);
+
+// echo add_coin('SOL', 123, 123);
+
+
+// print_r($coin_list);
+
+// ?>
