d2289d4ec3a7b6f225c9d3353ff6012712bf9d3c
RTGEvoNeon <mat8765@mail.ru>
2024-08-15 10:04:49 +0300

    change function of add_asset

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    require_once 'process.php'
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -5,18 +9,31 @@
     <title>Crypto-manager</title>
 </head>
 <body>
-
-    <h2>Введите свои данные</h2>
-    <form action="process.php" method="post">
+    <h1>Список транзакций:</h1>
+    <table border="1">
+        <tr>
+            <th>Монета</th>
+            <th>Цена покупки</th>
+            <th>Количество</th>
+        </tr>
+    <?
+    while ($row = $assets->fetch_assoc()) {
+        echo "<tr>";
+            echo "<td>" . $row["symbol"] . "</td>";
+            echo "<td>" . $row["purchase_price"] . "</td>";
+            echo "<td>" . $row["amount"] . "</td>";
+        echo "</tr>";
+    }?>
+    </table>   
+    
+    <h2>Добавить монету</h2>
+    <form action="/" method="post">
         <label for="name">Имя:</label>
         <input type="text" id="name" name="name" required><br><br>
-        
         <label for="price">Цена:</label>
         <input type="text" id="price" name="price" required><br><br>
-
         <label for="count">Количество:</label>
         <input type="text" id="count" name="count" required><br><br>
-        
         <input type="submit" value="Отправить">
     </form>
 </body>
--- process.php
+++ process.php
@@ -1,37 +1,31 @@
-
 <?php
-
-function add_coin(&$coin_list, $name, $average_price, $number_of_coins) {
-    if ($coin_list[$name]) {
-        $deposit = $coin_list[$name][1] * $coin_list[$name][0];
-        $new_deposit = $average_price * $number_of_coins;
-
-        $final_deposit = $deposit + $new_deposit;
-        $final_numbers_of_coins = $coin_list[$name][1] + $number_of_coins;
-        $final_average_price = $final_deposit / $final_numbers_of_coins;
-        $coin_list[$name] = [$final_average_price, $final_numbers_of_coins];
-    } else {
-        $coin_list[$name] = [$average_price, $number_of_coins];
-    } 
-    }
-    
-
-$file = 'coin_list.txt';
-$data = [];
-if (file_exists($file)) {
-    $data = json_decode(file_get_contents($file), true);
+$db = ['MySQL-5.7', 'root', '', "crypto"];
+$conn = mysqli_connect(...$db);
+if (!$conn) {
+    die("Подключение с базой данных не прошло!");
 }
 
-echo "Ваш портфель:";
-print_r($data);
+function add_asset($symbol, $price, $count) {
+    global $conn;
+    $name = "\"" . $symbol . "\"";
+    if ($res =  mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM coins WHERE symbol = $name"))) {    
+        $coin_id =  $res['id'];
+        $request =  "INSERT INTO assets(user_id, coin_id, amount, purchase_price) VALUES(1, $coin_id, $count, $price);";        
+        mysqli_query($conn, $request);    
+    } else {
+        $current_price = 100;
+        $query = "INSERT INTO coins(name, symbol, current_price) VALUES('$name', '$symbol', $current_price);";
+        mysqli_query($conn, $query);
+    }
+}
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $name = htmlspecialchars($_POST['name']);
     $price = htmlspecialchars($_POST['price']);
     $count = htmlspecialchars($_POST['count']);
-    add_coin($data, $name, $price, $count);
+    add_asset($name, $price, $count);
 }
 
-file_put_contents($file, json_encode($data));
-
+$assets = mysqli_query($conn, "SELECT coins.symbol, assets.purchase_price, assets.amount FROM assets JOIN coins ON assets.coin_id=coins.id WHERE user_id = 1;");
+mysqli_close($conn);
 ?>
