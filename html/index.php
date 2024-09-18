4d348e356e2948d4e5cee8db18b3f47176fe04e1
Ilya Kondryukov <mat8765@mail.ru>
2024-09-18 09:58:25 +0300

    обновил структуру проекта

--- html/index.php
+++ html/index.php
@@ -1,4 +1,5 @@
 <?php
+
 $dsn = 'mysql:host=db;dbname=crypto_db';
 
 $username = 'crypto_user';
@@ -14,7 +15,38 @@ try {
     echo "Connection failed: " . $e->getMessage();
 }
 
-$sql = 'SELECT * FROM coins';
-$stmt = $pdo->query($sql);
-$rows = $stmt->fetchAll();
-echo json_encode($rows, JSON_PRETTY_PRINT);
+// function add_transaction($symbol, $price, $amount)
+// {
+//     global $conn;
+//     if ($res =  pg_fetch_assoc(pg_query($conn, "SELECT * FROM coins WHERE symbol = '$symbol'"))) {
+//         $coin_id =  $res['id'];
+//         $request =  "INSERT INTO transactions(user_id, coin_id, amount, price) VALUES(1, $coin_id, $amount, $price);";
+//         pg_query($conn, $request);
+//     } else {
+//         $current_price = 40;
+//         $query = "INSERT INTO coins(name, symbol, current_price) VALUES('$name', '$symbol', $current_price);";
+//         pg_query($conn, $query);
+//     }
+// }
+
+// if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+//     $name = htmlspecialchars($_POST['name']);
+//     $price = htmlspecialchars($_POST['price']);
+//     $amount = htmlspecialchars($_POST['amount']);
+//     add_transaction($name, $price, $amount);
+// }
+$query = "SELECT * FROM balances WHERE user_id=2;";
+$balances = $pdo->query($query)->fetchAll();
+$total_balance = 0;
+$total_investment = 0;
+foreach ($balances as $balance) {
+    $total_balance += $balance["average_price"] * $balance["quantity"];
+    $total_investment += $balance["investment_amount"];
+}
+header('Content-Type: application/json');
+$response = [
+    "total_investment" => $total_investment,
+    "total_balance" => $total_balance,
+    "balances" => $balances,
+];
+echo json_encode($response, JSON_PRETTY_PRINT);
