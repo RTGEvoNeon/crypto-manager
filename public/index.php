ae1f285bc1be759ec64a4d1de10c228058725fc7
Ilya Kondryukov <mat8765@mail.ru>
2024-09-21 00:13:23 +0300

    Настроил структуру проекта

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Крипто-портфель</title>
+</head>
+
+<body>
+    <h1>Это только начало!</h1>
+    <form class="form" action="" method="post" style="display: block;">
+        <input class="input" type="text" placeholder="Имя">
+        <input class="input" type="text" placeholder="Фамилия">
+        <input class="input" type="email" placeholder="Почта">
+        <input class="input" type="password" placeholder="Пароль">
+        <input class="input" type="password" placeholder="Пароль еще раз">
+        <button class="btn" type="submit">Регистрация</button>
+    </form>
+</body>
